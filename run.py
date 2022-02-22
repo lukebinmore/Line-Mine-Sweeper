863683e52dbb5a94c68996f6aa5938be51726e83
Luke Binmore <lukebinmore@gmail.com>
2022-02-22 23:35:16 +0000

    Update - Draw Board Function

--- run.py
+++ run.py
@@ -31,8 +31,48 @@ class Board:
             [0 for column in range(self.size)] for row in range(self.size)
         ]
         self.grid_visable = [
-            [" " for column in range(self.size) for row in range(self.size)]
+            [" " for column in range(self.size)] for row in range(self.size)
         ]
+
+    def draw_board(self):
+        """
+        Adds stylings to each row of the visable grid.
+        Prints out visable grid row by row, using center_line function.
+        """
+
+        heading_top = (
+            "".join(["|  " + str(i + 1) + "  " for i in range(self.size)]) + "|"
+        )
+        print(center_line(heading_top))
+        print()
+
+        for i, row in enumerate(self.grid_visable):
+            new_line = ""
+
+            if i == 0:
+                new_line = "\u203E  "
+                new_line = (
+                    new_line
+                    + "".join(["|\u203E\u203E\u203E\u203E\u203E" for cell in row])
+                    + "|   "
+                )
+            else:
+                new_line = "   "
+                new_line = new_line + "".join(["|     " for cell in row]) + "|   "
+
+            print(center_line(new_line))
+
+            new_line = str(i + 1) + "  "
+            new_line = (
+                new_line + "".join(["|  " + str(cell) + "  " for cell in row]) + "|   "
+            )
+            print(center_line(new_line))
+
+            new_line = "_  "
+            new_line = new_line + "".join(["|_____" for cell in row]) + "|   "
+            print(center_line(new_line))
+
+
 # endregion
 
 # region Functions
@@ -40,8 +80,11 @@ def center_line(line):
     """
     Centers the given string based on the width of the terminal, set with the global WIDTH perameter.
     """
+
     new_line_spaces = int((WINDOW_WIDTH - len(line)) / 2)
     return "".join([" " for space in range(new_line_spaces)]) + line
+
+
 # endregion
 
 # region Main
@@ -51,7 +94,8 @@ def main():
     """
 
     game = Board(7)
-    print(center_line("testpoisdjfpoksdfo"))
+    game.draw_board()
+
 
 main()
 # endregion
