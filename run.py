e9e9dc53ec29cd1bd9c564b625f59fc64c407fa4
Luke Binmore <lukebinmore@gmail.com>
2022-02-22 19:34:14 +0000

    Update - Update Starting Docstrings

--- run.py
+++ run.py
@@ -4,20 +4,24 @@
 
 # region Game Board Class
 class Board:
-    """Minesweeper game class"""
+    """
+    Minesweeper game class.
+    """
 
     size = 7
     grid_hidden = []
     grid_visable = []
 
     def __init__(self, grid_size):
-        """A"""
-
         self.size = grid_size
         self.create_grids()
 
     def create_grids(self):
-        """A"""
+        """
+        Creates both the hidden and visable grids as nested lists.
+        Fills the newly created hidden grid with 0's.
+        Fills the newly created visable grid with spaces.
+        """
 
         self.grid_hidden = [
             [0 for column in range(self.size)] for row in range(self.size)
@@ -31,7 +35,9 @@ class Board:
 
 # region Main
 def main():
-    """Main function"""
+    """
+    Main function
+    """
 
     game = Board(7)
     print(game.grid_hidden)
