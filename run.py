b7693cbc16768944211026b7aa655844bf2d0592
Luke Binmore <lukebinmore@gmail.com>
2022-02-22 14:35:17 +0000

    Update - Class Creation

--- run.py
+++ run.py
@@ -2,12 +2,26 @@
 
 # endregion
 
-
 # region Game Board Class
+class Board:
+    """Minesweeper game class"""
+
+    size = 7
+    grid_hidden = []
+    grid_visable = []
+
+    def __init__(self, grid_size):
+        self.size = grid_size
+
 
 # endregion
 
-
 # region Main
+def main():
+    """Main function"""
 
+    game = Board(7)
+
+
+main()
 # endregion
